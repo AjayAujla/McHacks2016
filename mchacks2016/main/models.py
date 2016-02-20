68d92731c993c1302ccb9a6dba36d3f9bf3bb614
micamus <matthew.jo.henry@gmail.com>
2016-02-20 17:44:59 -0500

    models change test

--- mchacks2016/main/models.py
+++ mchacks2016/main/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=7)
+    
+class Preference(models.Model):
+    name = models.CharField(max_length=50)
+    users = models.ManyToManyField(User)
+    
+class Availability(models.Model):
+    
