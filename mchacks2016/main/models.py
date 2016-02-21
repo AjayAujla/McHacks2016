4030ff077eee69c4539cfcb18d0a8610e655586a
Monica Ung <monica.ung@mail.mcgill.ca>
2016-02-21 04:54:08 -0500

    Updated gitignore, added password to user model

--- mchacks2016/main/models.py
+++ mchacks2016/main/models.py
@@ -5,10 +5,10 @@ from django.db import models
 # Preferences are more general, eg : outdoors
 class Preference(models.Model):
     name = models.CharField(max_length=50)
-    
+
     def __str__(self):
         return self.name
-    
+
 class Availability(models.Model):
     start_date=models.DateTimeField('Available from')
     end_date = models.DateTimeField('Available to')
@@ -20,7 +20,8 @@ class User(models.Model):
     postal_code = models.CharField(max_length=7)
     preference = models.ManyToManyField(Preference)
     availability = models.ManyToManyField(Availability)
-    
+    password = models.CharField(max_length=20, null=True)
+
     def __str__(self):
         return self.user_name
 
@@ -28,10 +29,10 @@ class User(models.Model):
 class Activity(models.Model):
     name = models.CharField(max_length=50)
     preferences = models.ManyToManyField(Preference)
-    
+
     def __str__(self):
         return self.name
-    
+
 # For each planned activity, make a "group" to decide what activity and when
 class Group(models.Model):
     name = models.CharField(max_length=50)
@@ -40,16 +41,16 @@ class Group(models.Model):
     chosen_date = models.ForeignKey(Availability, on_delete=models.CASCADE, related_name='chosen_date', blank=True, null=True)
     suggested_activities = models.ManyToManyField(Activity, related_name='suggested_activities', blank=True, null=True)
     suggested_dates = models.ManyToManyField(Availability, related_name='suggested_dates', blank=True, null=True)
-    
+
     def __str__(self):
         return self.name
-    
+
 # Candidates belong to group and activity and store voter list
 class Candidate_Activity(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
     voters = models.ManyToManyField(User)
-    
+
 class Candidate_Availability(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     availability = models.ForeignKey(Availability, on_delete=models.CASCADE)
--- mchacks2016/main/urls.py
+++ mchacks2016/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^dashboard', views.dashboard, name='dashboard'),				# /dashboard/
+	url(r'^register', views.register, name='register'),				# /register/
 	url(r'^group/(?P<group_name>\w+)', views.group, name='group'),		# /group/[group_name]/
 	url(r'^', views.index, name='index'), 								# /
 ]
