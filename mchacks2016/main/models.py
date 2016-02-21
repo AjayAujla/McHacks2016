9d3a4e918f27fb8aacc0b18ee5c9f39f6553ea75
micamus <matthew.jo.henry@gmail.com>
2016-02-20 20:47:37 -0500

    add str field

--- mchacks2016/main/models.py
+++ mchacks2016/main/models.py
@@ -9,20 +9,32 @@ class User(models.Model):
     last_name = models.CharField(max_length=50)
     postal_code = models.CharField(max_length=7)
     
+    def __str__(self):
+        return self.user_name
+    
 # Preferences are more general, eg : outdoors
 class Preference(models.Model):
     name = models.CharField(max_length=50)
     user = models.ManyToManyField(User)
     
+    def __str__(self):
+        return self.name
+    
 class Availability(models.Model):
     start_date=models.DateTimeField('Available from')
     end_date = models.DateTimeField('Available to')
     user = models.ManyToManyField(User)
+    
+    def __str__(self):
+        return self.start_date
 
 # Activities are a specific event, eg : hiking Mont Royal
 class Activity(models.Model):
     name = models.CharField(max_length=50)
     preferences = models.ManyToManyField(Preference)
+    
+    def __str__(self):
+        return self.name
     
 # For each planned activity, make a "group" to decide what activity and when
 class Group(models.Model):
@@ -32,6 +44,9 @@ class Group(models.Model):
     chosen_date = models.ForeignKey(Availability, on_delete=models.CASCADE, related_name='chosen_date')
     suggested_activities = models.ManyToManyField(Activity, related_name='suggested_activities')
     suggested_dates = models.ManyToManyField(Availability, related_name='suggested_dates')
+    
+    def __str__(self):
+        return self.name
     
 # Candidates belong to group and activity and store voter list
 class Candidate_Activity(models.Model):
