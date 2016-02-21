b3c3b2919820ba879a4529a0d9c9c578554561f7
Monica Ung <monica.ung@mail.mcgill.ca>
2016-02-21 02:01:34 -0500

    Merge the design for group

--- mchacks2016/main/views.py
+++ mchacks2016/main/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse
-
 from django.shortcuts import render
+from main.models import *
 
 # Create your views here.
 
 def dashboard(request):
-	context = {
+	user = User.objects.get(user_name='Bob')	# user_name=request.user
 
+	context = {
+		'user': user,
+		'userAvailability': user.availability.all
 	}
+	
 	return render(request, 'main/dashboard.html', context)
 
 def group(request, group_name):
