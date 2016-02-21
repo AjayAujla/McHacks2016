443faf4bfec4f3b853934a3472e807ac1bb4f11e
micamus <matthew.jo.henry@gmail.com>
2016-02-21 03:00:00 -0500

    register changes

--- mchacks2016/main/views.py
+++ mchacks2016/main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from main.models import *
 
@@ -21,6 +21,25 @@ def group(request, group_name):
 
 	return render(request, 'main/group.html', context)
 
+def register(request):
+    context = {  
+    }
+    
+    if request.method == 'GET':
+        # display empty form
+        return render(request, 'main/register.html', context)
+    elif request.method == 'POST': 
+        # request is a POST, save user info to database
+        newUser = User()
+        newUser.user_name = request.POST['user_name']
+        newUser.first_name = request.POST['first_name']
+        newUser.last_name =request.POST['last_name']
+        newUser.postal_code =request.POST['postal_code']
+        newUser.password =request.POST['password']
+        newUser.save()
+        return render(request, 'main/dashboard.html', context)
+#    else:
+        #TODO: raise 404 or something
 def index(request):
 	context = {
 	}
